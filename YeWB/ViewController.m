a305e0f009e0206fadc9e687c6afc5f303c78884
WeibaYeQiang <yqiang1000@163.com>
2016-07-26 10:30:08 +0800

    上传测试

--- YeWB/ViewController.m
+++ YeWB/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    button.backgroundColor = [UIColor redColor];
+    [self.view addSubview:button];
 }
 
 - (void)didReceiveMemoryWarning {
